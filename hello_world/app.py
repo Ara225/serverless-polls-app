ac26ad2d9d6387965913ad3f7491707f8678da55
Anna Aitchison <anna11199810@hotmail.co.uk>
2020-06-10 15:34:16 +0100

    First commit

---
+++ hello_world/app.py
@@ -0,0 +1,41 @@
+import json
+
+# import requests
+import boto3
+import os
+from boto3.dynamodb.conditions import Key
+def lambda_handler(event, context):
+    """Sample pure Lambda function
+
+    Parameters
+    ----------
+    event: dict, required
+        API Gateway Lambda Proxy Input Format
+
+        Event doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html#api-gateway-simple-proxy-for-lambda-input-format
+
+    context: object, required
+        Lambda Context runtime methods and attributes
+
+        Context doc: https://docs.aws.amazon.com/lambda/latest/dg/python-context-object.html
+
+    Returns
+    ------
+    API Gateway Lambda Proxy Output Format: dict
+
+        Return doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html
+    """
+    dynamodb = boto3.resource('dynamodb')
+    
+    table = dynamodb.Table(os.environ["DDB_TABLE_NAME"])
+    response = table.query(
+        KeyConditionExpression=Key('id').eq(1)
+    )
+    print(response['Items'])
+    return {
+        "statusCode": 200,
+        "body": json.dumps({
+            "message": "hello world",
+            # "location": ip.text.replace("\n", "")
+        }),
+    }
